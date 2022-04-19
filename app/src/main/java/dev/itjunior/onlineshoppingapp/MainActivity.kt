829bf96e79d0f5a03105286a5211e4ab04efa2e9
Itamar Junior <itjunior@gmail.com>
2022-04-19 10:20:01 +0100

    Recycler View

--- app/src/main/java/dev/itjunior/onlineshoppingapp/MainActivity.kt
+++ app/src/main/java/dev/itjunior/onlineshoppingapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package dev.itjunior.onlineshoppingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openNewPageButton.setOnClickListener{
+            Toast.makeText(this,"Button Clicked",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, PostActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/dev/itjunior/onlineshoppingapp/PostActivity.kt
@@ -0,0 +1,12 @@
+package dev.itjunior.onlineshoppingapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PostActivity: AppCompatActivity(R.layout.activity_posts) {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}

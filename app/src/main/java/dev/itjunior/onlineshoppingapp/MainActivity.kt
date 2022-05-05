98adab83dabbda8643d793fddc4a7b4e53d1d513
Itamar Junior <itjunior@gmail.com>
2022-05-05 21:50:29 +0100

    Signin, Signup and Products cart working

--- app/src/main/java/dev/itjunior/onlineshoppingapp/MainActivity.kt
+++ app/src/main/java/dev/itjunior/onlineshoppingapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package dev.itjunior.onlineshoppingapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
